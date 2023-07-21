9e81fd638371bbdfc787f31366fa50ba9088fcba
4ydx <nathan.findley@autify.com>
2023-07-21 17:32:43 +0900

    Small refactor based on chatgpt

--- src/lib.rs
+++ src/lib.rs
@@ -12,11 +12,11 @@ const HEADER_END: &[u8; 4] = b"\r\n\r\n";
 
 impl Request {
     pub fn body(&self) -> Vec<u8> {
-        self.raw[self.headers_end..].to_vec()
+        self.raw[self.headers_end + HEADER_END.len()..].to_vec()
     }
 
     pub fn body_complete(&self) -> bool {
-        self.raw[self.headers_end..].len() == self.content_length
+        self.raw[self.headers_end + HEADER_END.len()..].len() == self.content_length
     }
 
     pub fn update(&mut self, data: &mut Vec<u8>) {
@@ -25,68 +25,64 @@ impl Request {
         if self.headers.is_empty() {
             let mut at = self.headers_end;
 
-            let mut found_header_end = false;
             while at < self.raw.len() {
                 if self.raw[at..].starts_with(HEADER_END) {
-                    found_header_end = true;
-                    self.headers_end = at + HEADER_END.len();
-
-                    // 'header_newlines' contains the "\r\n" indices where newlines occur
-                    let header_chunk = self.raw[0..at].to_vec();
-                    let mut header_newlines = header_chunk
-                        .windows(2)
-                        .enumerate()
-                        .filter(|(_, w)| w == b"\r\n")
-                        .map(|(i, _)| i)
-                        .collect::<Vec<_>>();
-                    header_newlines.push(header_chunk.len());
-
-                    // print!("{:#?}", values);
-
-                    // This first entry in header_newlines skips the HTTP version line
-                    let mut header_start = header_newlines[0] + LINE_END.len();
-                    for header_end in header_newlines[1..].iter() {
-                        match String::from_utf8(
-                            header_chunk[header_start..header_end.clone()].to_owned(),
-                        ) {
-                            Ok(s) => self.headers.push(s),
-                            Err(e) => self.header_errors.push(e),
-                        }
-
-                        if self.raw[header_start..header_end.clone()]
-                            .starts_with(b"Content-Length:")
-                        {
-                            let mut v = vec![];
-                            let mut collect = false;
-                            for val in self.raw[header_start..header_end.clone()].to_vec().iter() {
-                                if collect {
-                                    v.push(*val);
-                                }
-                                if val == &b':' {
-                                    collect = true;
-                                }
-                            }
-                            let num = match String::from_utf8(v) {
-                                Ok(s) => s,
-                                Err(e) => {
-                                    self.header_errors.push(e);
-                                    "0".to_owned()
-                                }
-                            };
-
-                            self.content_length = match num.trim().parse::<usize>() {
-                                Ok(i) => i,
-                                Err(_) => 0,
-                            };
-                        }
-                        header_start = header_end + LINE_END.len();
-                    }
+                    self.headers_end = at;
+                    self.parse_headers();
+                    break;
                 }
+                self.headers_end = at;
                 at += 1;
             }
-            if !found_header_end {
-                self.headers_end = at;
+        }
+    }
+
+    fn parse_headers(&mut self) {
+        // 'header_newlines' contains the "\r\n" indices where newlines occur
+        let header_chunk = self.raw[0..self.headers_end].to_vec();
+        let mut header_newlines = header_chunk
+            .windows(2)
+            .enumerate()
+            .filter(|(_, w)| w == b"\r\n")
+            .map(|(i, _)| i)
+            .collect::<Vec<_>>();
+        header_newlines.push(header_chunk.len());
+
+        // print!("{:#?}", values);
+
+        // This first entry in header_newlines skips the HTTP version line
+        let mut header_start = header_newlines[0] + LINE_END.len();
+        for header_end in header_newlines[1..].iter() {
+            match String::from_utf8(header_chunk[header_start..header_end.clone()].to_owned()) {
+                Ok(s) => self.headers.push(s),
+                Err(e) => self.header_errors.push(e),
             }
+
+            if self.raw[header_start..header_end.clone()].starts_with(b"Content-Length:") {
+                let mut v = vec![];
+                let mut collect = false;
+                for val in self.raw[header_start..header_end.clone()].to_vec().iter() {
+                    if collect {
+                        v.push(*val);
+                    }
+                    if val == &b':' {
+                        collect = true;
+                    }
+                }
+                let num = match String::from_utf8(v) {
+                    Ok(s) => s,
+                    Err(e) => {
+                        self.header_errors.push(e);
+                        "0".to_owned()
+                    }
+                };
+
+                self.content_length = match num.trim().parse::<usize>() {
+                    Ok(i) => i,
+                    Err(_) => 0,
+                };
+            }
+            header_start = header_end + LINE_END.len();
         }
     }
 }
