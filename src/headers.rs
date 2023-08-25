07c09504440d762ce5b47f8eae81a829f0a17511
4ydx <nathan.findley@autify.com>
2023-08-25 23:06:42 +0900

    Multi-line and encoded-word headers
    No tests yet for encoded-word headers though

---
+++ src/headers.rs
@@ -0,0 +1,118 @@
+use base64::{engine::general_purpose, Engine as _};
+use encoding::label::encoding_from_whatwg_label;
+use encoding::DecoderTrap;
+
+#[derive(Debug, Default)]
+pub struct Headers {
+    pub raw: Vec<String>,
+}
+
+#[derive(Debug, Default)]
+pub struct Header {
+    pub charset: String,
+    pub key: String,
+    pub value: String,
+    pub bytes: Vec<u8>,
+    pub error: String,
+}
+
+impl Headers {
+    pub fn at(&self, index: usize) -> Header {
+        let raw_header: &str = &self.raw[index].to_owned();
+        let length = raw_header.len();
+
+        let mut key: &str = "";
+        let mut value: &str = "";
+
+        for i in 0..length {
+            if key.len() > 0 {
+                if value.len() == 0 && raw_header.as_bytes()[i] != b' ' {
+                    value = &raw_header[i..]; // trim leading whitespace
+                }
+            }
+            if raw_header.as_bytes()[i] == b':' {
+                key = &raw_header[0..i];
+            }
+        }
+
+        // an empty key implies a "bad" header value
+        if key.len() == 0 {
+            return Header {
+                charset: "utf8".to_owned(),
+                key: key.to_owned(),
+                value: value.to_owned(),
+                bytes: raw_header.as_bytes().to_vec(),
+                error: "header key/value pair not found".to_owned(),
+            };
+        }
+
+        // strictly speaking an encoded-word requires a longer string
+        // but this is simply to prevent a panic later on in the code
+        if value.len() < 2 {
+            return Header {
+                charset: "utf8".to_owned(),
+                key: key.to_owned(),
+                value: value.to_owned(),
+                bytes: raw_header.as_bytes().to_vec(),
+                error: "".to_owned(),
+            };
+        }
+
+        // encoded-word
+        //
+        // https://www.rfc-editor.org/rfc/rfc2047#section-2
+        // encoded-word = "=?" charset "?" encoding "?" encoded-text "?="
+        let value_bytes = value.as_bytes();
+        if value_bytes[0] == b'='
+            && value_bytes[1] == b'?'
+            && value_bytes[value_bytes.len() - 2] == b'?'
+            && value_bytes[value_bytes.len() - 1] == b'='
+        {
+            let mut parts = raw_header.split('?');
+            let charset = parts.nth(1).unwrap();
+            let encoding = parts.nth(2);
+            let encoded_text = parts.nth(3).unwrap();
+
+            let mut header = Header::default();
+            header.charset = charset.to_owned();
+
+            let header: Header = match encoding {
+                Some("B") => {
+                    match &general_purpose::STANDARD_NO_PAD.decode(encoded_text) {
+                        Ok(bytes) => header.bytes = bytes.to_vec(),
+                        Err(e) => header.error = e.to_string(),
+                    };
+                    header.key = key.to_owned();
+
+                    match encoding_from_whatwg_label(charset) {
+                        Some(enc) => {
+                            match enc.decode(&header.bytes, DecoderTrap::Strict) {
+                                Ok(s) => header.value = s,
+                                Err(e) => header.error = e.to_string(),
+                            };
+                        }
+                        None => {
+                            header.error = format!("unsupported charset {}", charset).to_owned()
+                        }
+                    };
+
+                    header
+                }
+                // Some("Q") => quoted_printable::decode(encoded_text).unwrap(),
+                _ => {
+                    header.error = "unsupported encoding".to_owned();
+                    header
+                }
+            };
+            return header;
+        }
+
+        return Header {
+            charset: "utf8".to_owned(),
+            key: key.to_owned(),
+            value: value.to_owned(),
+            bytes: raw_header.as_bytes().to_vec(),
+            error: "".to_owned(),
+        };
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+mod headers;
+
 #[derive(Debug, Default)]
 pub struct Request {
-    pub headers: Vec<String>,
-    pub find_headers_end: usize,
+    pub headers: headers::Headers,
+    pub headers_end: usize,
     pub content_length: usize,
     pub raw: Vec<u8>,
     pub header_errors: Vec<std::string::FromUtf8Error>,
@@ -20,29 +22,29 @@ const HEADER_END: &[u8; 4] = b"\r\n\r\n";
 
 impl Request {
     pub fn body(&self) -> Vec<u8> {
-        self.raw[self.find_headers_end + HEADER_END.len()..].to_vec()
+        self.raw[self.headers_end + HEADER_END.len()..].to_vec()
     }
 
     pub fn body_complete(&self) -> bool {
-        if self.find_headers_end + HEADER_END.len() > self.raw.len() {
+        if self.headers_end + HEADER_END.len() > self.raw.len() {
             return false;
         }
-        self.raw[self.find_headers_end + HEADER_END.len()..].len() == self.content_length
+        self.raw[self.headers_end + HEADER_END.len()..].len() == self.content_length
     }
 
     pub fn update(&mut self, data: &mut Vec<u8>) {
         self.raw.append(data);
 
-        if self.headers.is_empty() {
-            let mut at = self.find_headers_end;
+        if self.headers.raw.is_empty() {
+            let mut at = self.headers_end;
 
             while at < self.raw.len() {
                 if self.raw[at..].starts_with(HEADER_END) {
-                    self.find_headers_end = at;
+                    self.headers_end = at;
                     self.parse_and_fill_headers();
                     break;
                 }
-                self.find_headers_end = at;
+                self.headers_end = at;
                 at += 1;
             }
         }
@@ -52,47 +54,72 @@ impl Request {
     // newline_indices entry is the first instance of \r\n in the self.raw data field.
     // The first instance is, of course, at the end of the 'GET / HTTP/1.1' line.
     fn parse_and_fill_headers(&mut self) {
-        let header_chunk = self.raw[0..self.find_headers_end].to_vec();
+        let header_chunk = self.raw[0..self.headers_end].to_vec();
+
         let mut newline_indices = header_chunk
             .windows(2)
             .enumerate()
-            .filter(|(_, w)| w == b"\r\n")
+            .filter(|(_, w)| w == LINE_END)
             .map(|(i, _)| i)
             .collect::<Vec<_>>();
         newline_indices.push(header_chunk.len());
 
-        let mut header_start = newline_indices[0] + LINE_END.len();
-        for header_end in newline_indices[1..].iter() {
-            match String::from_utf8(header_chunk[header_start..header_end.clone()].to_owned()) {
-                Ok(s) => self.headers.push(s),
-                Err(e) => self.header_errors.push(e),
+        let mut newline = newline_indices.iter();
+        let mut at = newline.next().unwrap();
+        loop {
+            let sindex = at + LINE_END.len();
+            let mut eindex = match newline.next() {
+                Some(eindex) => eindex,
+                None => break,
+            };
+
+            let mut skip: Vec<usize> = vec![sindex];
+            loop {
+                if eindex == &header_chunk.len() {
+                    break;
+                }
+
+                // evaluate the first byte in the next line
+                // to determine if we are dealing with a multi-line header
+                let next_byte = header_chunk[eindex + LINE_END.len()];
+                if next_byte == b'\t' || next_byte == b' ' {
+                    skip.push(*eindex);
+                    skip.push(eindex + LINE_END.len() + 1);
+
+                    eindex = match newline.next() {
+                        Some(eindex) => eindex,
+                        None => break,
+                    };
+                } else {
+                    break;
+                }
+            }
+            skip.push(*eindex);
+
+            // remove spaces in multi-line headers
+            let mut header: Vec<u8> = vec![];
+            for i in 0..skip.len() - 1 {
+                if i % 2 == 1 {
+                    continue;
+                }
+                let mut chunk = header_chunk[skip[i]..skip[i + 1]].to_owned();
+                header.append(&mut chunk);
             }
 
-            if self.raw[header_start..header_end.clone()].starts_with(b"Content-Length:") {
-                let mut v = vec![];
-                let mut collect = false;
-                for val in self.raw[header_start..header_end.clone()].to_vec().iter() {
-                    if collect {
-                        v.push(*val);
-                    }
-                    if val == &b':' {
-                        collect = true;
-                    }
-                }
-                let num = match String::from_utf8(v) {
-                    Ok(s) => s,
-                    Err(e) => {
-                        self.header_errors.push(e);
-                        "0".to_owned()
-                    }
-                };
+            match String::from_utf8(header.to_owned()) {
+                Ok(s) => self.headers.raw.push(s),
+                Err(e) => self.header_errors.push(e),
+            }
+            at = eindex;
 
-                self.content_length = match num.trim().parse::<usize>() {
+            // check most recent header to see if it contains content-length
+            let header = self.headers.at(self.headers.raw.len() - 1);
+            if header.key == "Content-Length" && header.error.len() == 0 {
+                self.content_length = match header.value.trim().parse::<usize>() {
                     Ok(i) => i,
                     Err(_) => 0,
                 };
             }
-            header_start = header_end + LINE_END.len();
         }
     }
 }
@@ -109,9 +136,9 @@ mod tests {
                 .as_bytes()
                 .to_vec(),
         );
-        assert_eq!(r.headers[0], "Content-Length: 4");
-        assert_eq!(r.headers[1], "Here: here");
-        assert_eq!(r.headers.len(), 2);
+        assert_eq!(r.headers.raw[0], "Content-Length: 4");
+        assert_eq!(r.headers.raw[1], "Here: here");
+        assert_eq!(r.headers.raw.len(), 2);
         assert_eq!(r.content_length, 4);
         assert_eq!(r.header_errors.len(), 0);
         assert_eq!(r.body(), vec![b'B', b'O', b'D', b'Y']);
@@ -126,9 +153,9 @@ mod tests {
                 .as_bytes()
                 .to_vec(),
         );
-        assert_eq!(r.headers[0], "Content-Length: 5");
-        assert_eq!(r.headers[1], "Here: here");
-        assert_eq!(r.headers.len(), 2);
+        assert_eq!(r.headers.raw[0], "Content-Length: 5");
+        assert_eq!(r.headers.raw[1], "Here: here");
+        assert_eq!(r.headers.raw.len(), 2);
         assert_eq!(r.content_length, 5);
         assert_eq!(r.body_complete(), false);
 
@@ -143,12 +170,25 @@ mod tests {
         assert_eq!(r.body_complete(), false);
 
         r.update(&mut "More: more\r\nFinal: final\r\n\r\n".as_bytes().to_vec());
-        assert_eq!(r.headers[0], "Here: here");
-        assert_eq!(r.headers[1], "More: more");
-        assert_eq!(r.headers[2], "Final: final");
-        assert_eq!(r.headers.len(), 3);
+        assert_eq!(r.headers.raw[0], "Here: here");
+        assert_eq!(r.headers.raw[1], "More: more");
+        assert_eq!(r.headers.raw[2], "Final: final");
+        assert_eq!(r.headers.raw.len(), 3);
         assert_eq!(r.content_length, 0);
         assert_eq!(r.header_errors.len(), 0);
+        assert_eq!(r.body_complete(), true);
+    }
+
+    #[test]
+    fn test_multi_line_header() {
+        let mut r = Request::default();
+        r.update(
+            &mut "GET / HTTP/1.1\r\nWrapping: wrapp\r\n ing\r\n\ttest\r\nAnother: a\r\n\r\n"
+                .as_bytes()
+                .to_vec(),
+        );
+        assert_eq!(r.headers.raw[0], "Wrapping: wrappingtest");
+        assert_eq!(r.headers.raw[1], "Another: a");
         assert_eq!(r.body_complete(), true);
     }
 }
