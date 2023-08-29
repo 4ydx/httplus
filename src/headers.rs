5e0d9b8c48c8e412b904bc0a3d84910428442b49
4ydx <nathan.findley@autify.com>
2023-08-29 17:17:32 +0900

    Check for non-ascii bytes

--- src/headers.rs
+++ src/headers.rs
@@ -3,9 +3,8 @@ pub struct Headers {
     pub raw: Vec<String>,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq)]
 pub struct Header {
-    pub charset: String,
     pub key: String,
     pub value: String,
     pub bytes: Vec<u8>,
@@ -27,7 +26,6 @@ impl Headers {
     pub fn at(&self, index: usize) -> Header {
         if index >= self.len() {
             return Header {
-                charset: "utf8".to_owned(),
                 key: "".to_owned(),
                 value: "".to_owned(),
                 bytes: vec![],
@@ -35,19 +33,32 @@ impl Headers {
             };
         }
 
-        let raw_header: &str = &self.raw[index].to_owned();
+        let raw_header: &str = &self.raw[index];
         let length = raw_header.len();
 
         let mut key: &str = "";
         let mut value: &str = "";
 
         for i in 0..length {
+            let byte = raw_header.as_bytes()[i];
+
+            if byte > 127 {
+                return Header {
+                    key: "".to_owned(),
+                    value: "".to_owned(),
+                    bytes: raw_header.as_bytes().to_vec(),
+                    error: format!("non-ascii byte found at index {}", i).to_owned(),
+                };
+            }
+
             if key.len() > 0 {
-                if value.len() == 0 && raw_header.as_bytes()[i] != b' ' {
-                    value = &raw_header[i..]; // trim leading whitespace
+                // trim leading whitespace
+                if value.len() == 0 && byte != b' ' {
+                    value = &raw_header[i..];
                 }
             }
-            if raw_header.as_bytes()[i] == b':' {
+
+            if byte == b':' {
                 key = &raw_header[0..i];
             }
         }
@@ -55,7 +66,6 @@ impl Headers {
         // an empty key implies a "bad" header value
         if key.len() == 0 {
             return Header {
-                charset: "utf8".to_owned(),
                 key: key.to_owned(),
                 value: value.to_owned(),
                 bytes: raw_header.as_bytes().to_vec(),
@@ -63,24 +73,41 @@ impl Headers {
             };
         }
 
-        // strictly speaking an encoded-word requires a longer string
-        // but this is simply to prevent a panic later on in the code
-        if value.len() < 2 {
-            return Header {
-                charset: "utf8".to_owned(),
-                key: key.to_owned(),
-                value: value.to_owned(),
-                bytes: raw_header.as_bytes().to_vec(),
-                error: "".to_owned(),
-            };
-        }
-
         return Header {
-            charset: "utf8".to_owned(),
             key: key.to_owned(),
             value: value.to_owned(),
             bytes: raw_header.as_bytes().to_vec(),
             error: "".to_owned(),
         };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_non_ascii() {
+        let h = Headers {
+            raw: vec!["foo: bar".to_owned(), "foo: bär".to_owned()],
+        };
+        assert_eq!(
+            Header {
+                bytes: "foo: bar".as_bytes().to_vec(),
+                key: "foo".to_owned(),
+                value: "bar".to_owned(),
+                error: "".to_owned(),
+            },
+            h.at(0)
+        );
+        assert_eq!(
+            Header {
+                bytes: "foo: bär".as_bytes().to_vec(),
+                key: "".to_owned(),
+                value: "".to_owned(),
+                error: "non-ascii byte found at index 6".to_owned(),
+            },
+            h.at(1)
+        );
     }
 }
